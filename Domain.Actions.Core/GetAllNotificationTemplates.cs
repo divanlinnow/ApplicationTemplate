b178ffcdca070514686e39ce773a3ecb634c32c5
Divan Linnow <divan.linnow@gmail.com>
2018-06-21 19:59:35 +0200

    Added more Core Actions

---
+++ Domain.Actions.Core/GetAllNotificationTemplates.cs
@@ -0,0 +1,40 @@
+﻿using ApplicationFramework.Notifications;
+using Domain.Actions.Core.Services;
+using Domain.Actions.Core.ViewModels;
+using Domain.Models.Core;
+using System;
+
+namespace Domain.Actions.Core
+{
+    public sealed class GetAllNotificationTemplates<T> : BasicAction<T> where T : class
+    {
+        public GetAllNotificationTemplates(IClientServicesProvider clientServices) : base(clientServices)
+        {
+        }
+
+        public Func<GenericListViewModel<NotificationTemplateDto>, T> OnComplete { get; set; }
+
+        public T Invoke()
+        {
+            return Execute(() =>
+            {
+                var model = new GenericListViewModel<NotificationTemplateDto>();
+
+                var serviceResult = ClientServices.NotificationTemplateService.GetAllNotificationTemplates();
+
+                if (serviceResult == null || serviceResult.Result == null || serviceResult.Notifications.HasErrors())
+                {
+                    var errorMessage = "Sorry, an unexpected error occurred.";
+                    model.Notifications.AddError(errorMessage);
+                }
+                else
+                {
+                    model.Items = serviceResult.Result;
+                }
+
+                return OnComplete(model);
+
+            }, this.GetType().Name);
+        }
+    }
+}
---
+++ Domain.Actions.Core/UpdateNotificationTemplate.cs
@@ -0,0 +1,40 @@
+﻿using ApplicationFramework.Notifications;
+using Domain.Actions.Core.Services;
+using Domain.Actions.Core.ViewModels;
+using Domain.Models.Core;
+using System;
+
+namespace Domain.Actions.Core
+{
+    public sealed class UpdateNotificationTemplate<T> : BasicAction<T> where T : class
+    {
+        public UpdateNotificationTemplate(IClientServicesProvider clientServices) : base(clientServices)
+        {
+        }
+
+        public Func<GenericViewModel, T> OnComplete { get; set; }
+
+        public T Invoke(NotificationTemplateDto notificationTemplate)
+        {
+            return Execute(() =>
+            {
+                var model = new GenericViewModel();
+
+                var serviceResult = ClientServices.NotificationTemplateService.UpdateNotificationTemplate(notificationTemplate);
+
+                if (serviceResult == null || serviceResult.Notifications.HasErrors())
+                {
+                    var errorMessage = "Sorry, an unexpected error occurred.";
+                    model.Notifications.AddError(errorMessage);
+                }
+                else
+                {
+                    model.Success = serviceResult.Result;
+                }
+
+                return OnComplete(model);
+
+            }, this.GetType().Name);
+        }
+    }
+}
