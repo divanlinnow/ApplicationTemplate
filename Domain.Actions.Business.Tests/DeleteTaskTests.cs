a3e53a0af6cc64113b2965c46f7e179d3e5e4d45
Divan Linnow <divan@haefelesoftware.com>
2018-07-10 14:06:54 +0200

    Added unit tests for Business Actions.

---
+++ Domain.Actions.Business.Tests/DeleteTaskTests.cs
@@ -0,0 +1,97 @@
+﻿using ApplicationFramework.Logging;
+using ApplicationFramework.Notifications;
+using Domain.Models.Business.Tests;
+using Domain.Services.Business.ServiceProvider;
+using Domain.Services.Core;
+using Domain.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+
+namespace Domain.Actions.Business.Tests
+{
+    [TestClass]
+    public class DeleteTaskTests
+    {
+        private Mock<ILogger> mockLogger;
+        private Mock<IServiceProviderBusiness> mockClientServicesProvider;
+
+        [TestInitialize]
+        public void Init()
+        {
+            mockLogger = new Mock<ILogger>();
+            mockClientServicesProvider = new Mock<IServiceProviderBusiness>();
+        }
+
+        [TestMethod]
+        [TestCategory("Actions - Business")]
+        public void DeleteTask_Action_Success()
+        {
+            // Arrange
+            var taskDto = TestHelper.TaskDto();
+
+            var fakeResponse = new GenericServiceResponse<bool>
+            {
+                Result = true
+            };
+
+            mockClientServicesProvider.Setup(x => x.Logger).Returns(mockLogger.Object).Verifiable();
+            mockClientServicesProvider.Setup(x => x.TaskService.DeleteTask(taskDto)).Returns(fakeResponse).Verifiable();
+
+            var viewModel = new GenericViewModel();
+
+            var action = new DeleteTask<GenericViewModel>(mockClientServicesProvider.Object)
+            {
+                OnComplete = model => viewModel = model
+            };
+
+            // Act
+            var result = action.Invoke(taskDto);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(GenericViewModel));
+            Assert.IsNotNull(result.Notifications);
+            Assert.IsInstanceOfType(result.Notifications, typeof(NotificationCollection));
+            Assert.IsTrue(result.Notifications.Count() == 0);
+            Assert.IsFalse(result.HasErrors);
+            Assert.IsNotNull(result.Success);
+            Assert.IsInstanceOfType(result.Success, typeof(bool));
+            Assert.IsTrue(result.Success);
+        }
+
+        [TestMethod]
+        [TestCategory("Actions - Business")]
+        public void DeleteTask_Action_Fails()
+        {
+            // Arrange
+            var taskDto = TestHelper.TaskDto();
+
+            GenericServiceResponse<bool> fakeResponse = null;
+
+            mockClientServicesProvider.Setup(x => x.Logger).Returns(mockLogger.Object).Verifiable();
+            mockClientServicesProvider.Setup(x => x.TaskService.DeleteTask(taskDto)).Returns(fakeResponse).Verifiable();
+
+            var viewModel = new GenericViewModel();
+
+            var action = new DeleteTask<GenericViewModel>(mockClientServicesProvider.Object)
+            {
+                OnComplete = model => viewModel = model
+            };
+
+            // Act
+            var result = action.Invoke(taskDto);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(GenericViewModel));
+            Assert.IsNotNull(result.Notifications);
+            Assert.IsInstanceOfType(result.Notifications, typeof(NotificationCollection));
+            Assert.IsTrue(result.Notifications.Count() == 1);
+            Assert.IsTrue(result.HasErrors);
+            Assert.IsNotNull(result.Success);
+            Assert.IsInstanceOfType(result.Success, typeof(bool));
+            Assert.IsFalse(result.Success);
+        }
+    }
+}
---
+++ Domain.Actions.Business.Tests/DeleteWorkflowTests.cs
@@ -0,0 +1,97 @@
+﻿using ApplicationFramework.Logging;
+using ApplicationFramework.Notifications;
+using Domain.Models.Business.Tests;
+using Domain.Services.Business.ServiceProvider;
+using Domain.Services.Core;
+using Domain.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+
+namespace Domain.Actions.Business.Tests
+{
+    [TestClass]
+    public class DeleteWorkflowTests
+    {
+        private Mock<ILogger> mockLogger;
+        private Mock<IServiceProviderBusiness> mockClientServicesProvider;
+
+        [TestInitialize]
+        public void Init()
+        {
+            mockLogger = new Mock<ILogger>();
+            mockClientServicesProvider = new Mock<IServiceProviderBusiness>();
+        }
+
+        [TestMethod]
+        [TestCategory("Actions - Business")]
+        public void DeleteWorkflow_Action_Success()
+        {
+            // Arrange
+            var workflowDto = TestHelper.WorkflowDto();
+
+            var fakeResponse = new GenericServiceResponse<bool>
+            {
+                Result = true
+            };
+
+            mockClientServicesProvider.Setup(x => x.Logger).Returns(mockLogger.Object).Verifiable();
+            mockClientServicesProvider.Setup(x => x.WorkflowService.DeleteWorkflow(workflowDto)).Returns(fakeResponse).Verifiable();
+
+            var viewModel = new GenericViewModel();
+
+            var action = new DeleteWorkflow<GenericViewModel>(mockClientServicesProvider.Object)
+            {
+                OnComplete = model => viewModel = model
+            };
+
+            // Act
+            var result = action.Invoke(workflowDto);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(GenericViewModel));
+            Assert.IsNotNull(result.Notifications);
+            Assert.IsInstanceOfType(result.Notifications, typeof(NotificationCollection));
+            Assert.IsTrue(result.Notifications.Count() == 0);
+            Assert.IsFalse(result.HasErrors);
+            Assert.IsNotNull(result.Success);
+            Assert.IsInstanceOfType(result.Success, typeof(bool));
+            Assert.IsTrue(result.Success);
+        }
+
+        [TestMethod]
+        [TestCategory("Actions - Business")]
+        public void DeleteWorkflow_Action_Fails()
+        {
+            // Arrange
+            var workflowDto = TestHelper.WorkflowDto();
+
+            GenericServiceResponse<bool> fakeResponse = null;
+
+            mockClientServicesProvider.Setup(x => x.Logger).Returns(mockLogger.Object).Verifiable();
+            mockClientServicesProvider.Setup(x => x.WorkflowService.DeleteWorkflow(workflowDto)).Returns(fakeResponse).Verifiable();
+
+            var viewModel = new GenericViewModel();
+
+            var action = new DeleteWorkflow<GenericViewModel>(mockClientServicesProvider.Object)
+            {
+                OnComplete = model => viewModel = model
+            };
+
+            // Act
+            var result = action.Invoke(workflowDto);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(GenericViewModel));
+            Assert.IsNotNull(result.Notifications);
+            Assert.IsInstanceOfType(result.Notifications, typeof(NotificationCollection));
+            Assert.IsTrue(result.Notifications.Count() == 1);
+            Assert.IsTrue(result.HasErrors);
+            Assert.IsNotNull(result.Success);
+            Assert.IsInstanceOfType(result.Success, typeof(bool));
+            Assert.IsFalse(result.Success);
+        }
+    }
+}
